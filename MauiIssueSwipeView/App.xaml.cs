29d6f2a44b94a937ea6245fdaf39351bfa55ad22
Heinz Holzer <holzerheinz@hotmail.com>
2023-09-05 07:53:29 +0200

    Add project files.

---
+++ MauiIssueSwipeView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiIssueSwipeView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiIssueSwipeView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiIssueSwipeView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
